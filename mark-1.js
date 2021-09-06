0eb7eb16a5acb3a0246b36d6fe3ac907b739040b
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-09-06 14:42:46 +0530

    edits

---
+++ mark-1.js
@@ -0,0 +1,13 @@
+//mark-1
+
+//EX- 01
+
+console.log("siddharth");
+
+
+//EX- 02
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log("Hi " + userName);
