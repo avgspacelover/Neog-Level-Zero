eeab379d1354604e94a96a6b5958aa1e84422ca5
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-09-07 00:27:03 +0530

    edits

---
+++ m1-exercises.js
@@ -0,0 +1,45 @@
+var readlineSync = require('readline-sync');
+
+
+
+var questionbank= [
+    {
+      question:"Name the only protagonist in the show w/o a degree",
+      answer: "mike",
+    },
+    {
+      question:"Which of the love interests of Harvey had the most conflicting relationship?",
+      answer: "scottie",
+    },
+    {
+      question:"Who ousted Mike to the US govermment?",
+      answer:"sheila",
+    },
+    {
+      question:"Who was Harvey's arch nemesis",
+      answer:"tanner",
+    },
+    {
+      question:"who had a history of always ducking Harvey?",
+      answer:"elliot",
+    },
+    {
+      question:"In which season were Mike and Harvey on opposite sides of a hostile takeover battle?",
+      answer:"4",
+    },
+    {
+      question:"who uses Mike's secret to get their name on the wall of the firm?",
+      answer:"louis",
+    },
+    {
+      question:"which Law School does Rachel go to?",
+      answer:"columbia",    
+    },
+    {
+      question:"who is harvey's best friend in SEC?(last name will do)",
+      answer:"cahill",
+    },
+    {
+      question:"who was the opposing counsel in the ultimate criminal lawsuit against Mike(last name will do)",
+      answer:"gibbs",
+    }]
