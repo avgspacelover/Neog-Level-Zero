6ec819eb84684b34136d162924210ed74ec8fb8a
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-09-08 11:41:59 +0530

    refactor m2.js

--- mark-2.js
+++ mark-2.js
@@ -2,7 +2,7 @@ var readlineSync = require('readline-sync');
 
 
 
-var questionbank= [
+var quespool= [
     {
       question:"Name the only protagonist in the show w/o a degree",
       answer: "mike",
@@ -43,3 +43,45 @@ var questionbank= [
       question:"who was the opposing counsel in the ultimate criminal lawsuit against Mike(last name will do)",
       answer:"gibbs",
     }]
+
+
+    var countit=0;
+
+for (var i=0; i<quespool.length ;i++){
+    var ques= quespool[i].question;
+    var answ= quespool[i].answer;
+    quizxx(ques,answ);
+
+}
+var highscore= [];
+var max=0;
+
+for (var i=0; i<highscore.length;i++){
+    if (highscore[i]>max){
+        max=highscore[i];
+    }
+}
+
+if(countit>max){
+    console.log("Congrats, you set the high score!");
+}
+
+
+highscore.push(countit); 
+
+
+function quizxx(ques,answ){
+        
+    var userans=readlineSync.question(ques);
+    if(userans.toLowerCase==answ){
+        countit++;
+        console.log("Correct!\nScore(till now): "+ countit);
+
+    } else {
+        countit--;
+        console.log("Wrong!\nScore(till now): "+ countit);
+    }
+
+
+
+}
