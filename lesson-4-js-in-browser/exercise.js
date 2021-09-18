e03bd634cc0460d684a4dbc76ebfc0833c8b5b65
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-09-18 18:16:10 +0530

    added functionality to read input

---
+++ lesson-4-js-in-browser/exercise.js
@@ -0,0 +1,32 @@
+//exercise-2
+
+var name= prompt("Tell your name");
+
+var answer= "Welcome here, " + name;
+
+alert(answer);
+
+//exercise-3
+
+var translatebtn= document.querySelector("#translatebtn");
+
+translatebtn.addEventListener("click", function clickevent(){
+    console.log("clicked");
+})
+
+//exercise-4
+var translatebtn= document.querySelector("#translatebtn");
+var input= document.querySelector("#text-input");
+
+translatebtn.addEventListener("click", function clickevent(){
+    console.log("clicked");
+    console.log("input",input.value)
+})
+
+//live-exercise
+
+// 1-("#textInput")
+// 2-(".btn-primary") 
+// 3-("#input-btn) 
+// 4-("[name=translator]")
+
--- lesson-4-js-in-browser/script.js
+++ lesson-4-js-in-browser/script.js
@@ -1,1 +1,13 @@
 // https://replit.com/@tanaypratap/lessonfourapi
+
+
+var translatebtn= document.querySelector("#translatebtn");
+var input= document.querySelector("#text-input");
+
+translatebtn.addEventListener("click", function clickevent(){
+    console.log("clicked");
+    console.log("input",input.value)
+})
+
+
+
