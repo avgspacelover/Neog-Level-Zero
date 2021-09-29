a0de342a407214ccdd611d17aa5d8af2a75fb7d7
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-09-29 17:14:43 +0530

    completed mark 7

--- mark-7(js in browser)/script.js
+++ mark-7(js in browser)/script.js
@@ -6,7 +6,7 @@ var translatebtn= document.querySelector("#translatebtn");
 var input= document.querySelector("#text-input");
 var output= document.querySelector("#output");
 
-var url ="https://funtranslations.com/translate/minion.json";
+var url ="https://api.funtranslations.com/translate/ferb-latin.json";
 
 function miniontranslate(text){
     return url + "?" + "text=" + text;
