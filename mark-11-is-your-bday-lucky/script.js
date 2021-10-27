e6a40f4f8448b7079cb45586eb44df5ee84eb005
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-10-27 06:25:09 +0530

    completed m11

--- mark-11-is-your-bday-lucky/script.js
+++ mark-11-is-your-bday-lucky/script.js
@@ -5,26 +5,47 @@
 
  const checkNumberBtn = document.querySelector("#check");
 
+ const outputBox = document.querySelector("#op-box");
+
  /* Testing input reception
  checkNumberBtn.addEventListener("click", function getValues(){
      console.log(typeof dateOfBirth.value, luckyNumber.value);
  })
  */
+ 
 
 
- function checkNumberIsLucky(){
+
+
+ function comparevalues(sum,luckyNumber){
+     if(sum%luckyNumber==0){
+         outputBox.innerText="Your Birthday is Lucky!";
+
+     } else{
+        outputBox.innerText="Your birthday is not Lucky";
+     }
+
+ }
+
+ function checkBirthdayIsLucky(){
      const bdate=dateOfBirth.value;
+     console.log(bdate);
      const sum = calculateSum(bdate);
-     console.log(sum);
+     if(sum&&bdate){
+         comparevalues(sum,luckyNumber.value);
+     } else{
+         outputBox.innerText="Please enter both the fields!!!!";
+     }
  }
 
  function calculateSum(bdate){
      bdate= bdate.replaceAll("-","");
+     console.log(bdate);
      let sum=0;
 
      for(let i=0; i<bdate.length; i++){
 
-        sum += Number(bdate.charAt(index))
+        sum = sum + Number(bdate.charAt(i));   // typecasting string => number
 
      }
 
@@ -32,6 +53,4 @@
 
  }
 
- checkNumberBtn.addEventlistener(){
-
- }
+ checkNumberBtn.addEventListener('click', checkBirthdayIsLucky);
