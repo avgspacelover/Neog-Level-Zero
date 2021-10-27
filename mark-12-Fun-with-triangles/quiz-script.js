e2d49958e68b85a786cbb0f2ce73d964f208f9b1
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-10-27 17:36:27 +0530

    edits

---
+++ mark-12-Fun-with-triangles/quiz-script.js
@@ -0,0 +1,18 @@
+ 
+ const quizForm = document.querySelector("#quiz-form");
+ 
+ const submitAnswerBtn = document.querySelector("#submit-btn");
+
+ const output = document.querySelector("output");
+
+ const correctAnswers = ["90°","Right angled"];
+
+
+ function calculateScore() {
+
+
+ }
+
+ submitAnswerBtn.addEventListener('click', calculateScore);
+
+ 
