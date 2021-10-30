2c8f7ad80d777df9ef37428243a19da90d1176aa
Siddharth Ram <siddharth.asterisk@gmail.com>
2021-10-30 16:18:29 +0530

    edits

---
+++ mark-13-is-your-bday-a-palindrome/script.js
@@ -0,0 +1,86 @@
+ 
+ const bdate = document.querySelector("#birthday");
+ 
+ const palBtn = document.querySelector("#palindrome-btn");
+
+ const output = document.querySelector("#output");
+
+
+
+
+
+
+ var flag=0;
+ var diff=0;
+
+ function depreciateDate(dates){ // 01-01-2022 => 01-23-4567
+
+    if()
+
+
+
+
+ }
+
+
+ function reverseString(arr){
+     
+
+    const temp=arr;
+    var newArr = [];
+
+    for(let i=0; i<= arr.length;i++){
+        newArr[i]=arr[(arr.length-i)];
+    }
+    var flag=1;
+    for(let i=0; i<= arr.length;i++){
+        if(newArr[i]!=arr[i]){
+            flag=0;
+            break;
+
+        }
+    }
+
+    if (flag){
+        return true;
+    }
+ }
+
+
+ function palindrome(arr){
+     
+    arr= number(arr);
+    
+    if(reverseString(arr)){
+
+      output.innerText = "Your Birthday is a Palindrome!";
+
+    } else{
+        var val=0;
+
+        while(val){
+            
+            depreciateDate(arr);
+
+            val=reverseString(arr);
+
+        }
+
+        output.innerText = "Sorry! Your Birthday is not a Palindrome. You missed by " + diff +" days.";
+
+       
+        
+
+    }
+
+ }
+
+ function dateToString(bdate){
+
+    const array=str.replaceAll("-","");
+    
+    palindrome(array);
+
+ }
+
+ palBtn.addEventListener("click", dateToString);
