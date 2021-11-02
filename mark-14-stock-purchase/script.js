e90bde8cab519536e55db21226a326517f167774
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-11-03 00:09:58 +0530

    completed mark 14

--- mark-14-stock-purchase/script.js
+++ mark-14-stock-purchase/script.js
@@ -9,3 +9,40 @@
  var btn = document.querySelector("#btn");
 
  var output = document.querySelector("#output");
+
+
+ function calculateProfitnLoss(initialPrice,quantity,finalPrice){
+
+    if(finalPrice>initialPrice){
+
+        var profit = ((finalPrice-initialPrice)*quantity);
+
+        var profitPerc = ((profit/initialPrice)*100);
+
+    } else if(initialPrice>finalPrice){
+
+        var loss = ((initialPrice-finalPrice)*quantity);
+
+        var lossPerc = ((loss/initialPrice)*100);
+
+    } else{
+
+        output.innerText = "No Pain, No Gain and No Gain, No Pain!";
+
+    }
+
+ }
+
+ function clickHandler(){
+
+    var initPrice = Number(price.value);
+
+    var qtty = Number(qty.value);
+
+    var cp = Number(curPrice.value);
+
+    calculateProfitnLoss(initPrice,qtty,cp);
+ }
+
+
+ btn.addEventListener('click', clickHandler);
