f686ba89258bd73258f44d96cc188add2d1ddd8b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-11-02 23:43:28 +0530

    edits

---
+++ mark-14-stock-purchase/script.js
@@ -0,0 +1,11 @@
+ 
+
+ var price = document.querySelector("#price");
+
+ var qty = document.querySelector("#qty");
+
+ var curPrice = document.querySelector("#cur-price");
+ 
+ var btn = document.querySelector("#btn");
+
+ var output = document.querySelector("#output");
