e84671c1754d8998d52863cfbe40e6c6b623a4eb
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-12-07 18:36:00 +0530

    edits

--- Lesson-5-Mark-9-Recom-sys/my-app/src/App.js
+++ Lesson-5-Mark-9-Recom-sys/my-app/src/App.js
@@ -2,98 +2,69 @@ import React from "react";
 import "./App.css";
 //, { useState }
 
-// const tedTalks = {
-//   productivity: [
-//     { name: "How to Get Your Brain to Focus"
-//     },
-//     { name: "The first 20 hours -- how to learn anything | Josh Kaufman | TEDxCSU"
-//     },
-//     { name: "The Secret of Becoming Mentally Strong | Amy Morin | TEDxOcala"
-//     },
-//     { name: "Why We Fear And Hold Back From What We Want | Stuti Singh | TEDxSevenoaks"
-//     },
-//     { name: "Inside the mind of a master procrastinator | Tim Urban"
-//     }
-//   ],
+const tedTalks = {
+  productivity: [
+    { name: "How to Get Your Brain to Focus"
+    },
+    { name: "The first 20 hours -- how to learn anything | Josh Kaufman | TEDxCSU"
+    },
+    { name: "The Secret of Becoming Mentally Strong | Amy Morin | TEDxOcala"
+    },
+    { name: "Why We Fear And Hold Back From What We Want | Stuti Singh | TEDxSevenoaks"
+    },
+    { name: "Inside the mind of a master procrastinator | Tim Urban"
+    }
+  ],
 
-//   depression: [
-//     { name: "I Am Depressed | Dianna Paige | TEDxAllendaleColumbiaSchool"
-//     },
-//     { name: "This could be why you're depressed or anxious | Johann Hari"
-//     },
-//     { name: "Lift Depression With These 3 Prescriptions- Without-Pills | Susan Heitler | TEDxWilmington"
-//     }
-//   ],
-//   aesthetics: [
-//     { name: "Looks aren't everything. Believe me, I'm a model. | Cameron Russell"
-//     },
-//     { name: "What makes you special? | Mariana Atencio | TEDxUniversityofNevada "
-//     },
-//     { name: "Why You Think You're Ugly | Melissa Butler | TEDxDetroit"
-//     }
-//   ],
-//   entrepreneurship: [
-//     { name: "Entrepreneurship As A State Of Mind | Mr. Ankur Warikoo | TEDxSBSC"
-//     },
-//     { name: "How I became the youngest entrepreneur | Tilak Mehta | TEDxGSMC"
-//     },
-//     { name: "What they don't tell you about entrepreneurship | Mark Leruste | TEDxCardiff"
-//     }
-//   ]
-// };
+  depression: [
+    { name: "I Am Depressed | Dianna Paige | TEDxAllendaleColumbiaSchool"
+    },
+    { name: "This could be why you're depressed or anxious | Johann Hari"
+    },
+    { name: "Lift Depression With These 3 Prescriptions- Without-Pills | Susan Heitler | TEDxWilmington"
+    }
+  ],
+  aesthetics: [
+    { name: "Looks aren't everything. Believe me, I'm a model. | Cameron Russell"
+    },
+    { name: "What makes you special? | Mariana Atencio | TEDxUniversityofNevada "
+    },
+    { name: "Why You Think You're Ugly | Melissa Butler | TEDxDetroit"
+    }
+  ],
+  entrepreneurship: [
+    { name: "Entrepreneurship As A State Of Mind | Mr. Ankur Warikoo | TEDxSBSC"
+    },
+    { name: "How I became the youngest entrepreneur | Tilak Mehta | TEDxGSMC"
+    },
+    { name: "What they don't tell you about entrepreneurship | Mark Leruste | TEDxCardiff"
+    }
+  ]
+};
 
-// var emojiLib = Object.keys(emojiDictionary);
+var emojiLib = Object.keys(emojiDictionary);
 
 export default function App() {
-  // const [meaning, setMeaning] = useState("");
+  const [meaning, setTheme] = useState("productivity");
 
-  // var heading = "Emoji Pedia";
+  function themeSelector(theme){
+    setTheme(theme);
 
-  // function emojiInteract(event) {
-  //   var userInput = event.target.value;
+  }
 
-  //   var meaning = emojiDictionary[userInput];
-
-  //   if (meaning === undefined) {
-  //     meaning = "Unavailable in the database :(";
-  //   }
-  //   setMeaning(meaning);
-  // }
-
-  // function emojiClickHandle(emoji) {
-  //   var meaning = emojiDictionary[emoji];
-  //   console.log(emoji);
-  //   setMeaning(meaning);
-  // }
+  function emojiClickHandle(emoji) {
+    var meaning = emojiDictionary[emoji];
+    console.log(emoji);
+    setMeaning(meaning);
+  }
 
   function listInteract(theme){
     console.log(theme);
   }
 
   return (
-    <div className="Emoji-App">
-      {/* 
-
-      
-
-      
-
-      <h2 style={{ fontWeight: "bold" }}> {meaning}</h2>
-
-      
-
-      {emojiLib.map(function (emoji) {
-        return (
-          <span
-            onClick={() => emojiClickHandle(emoji)}
-            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-            key={emoji}
-          >
-            {emoji}
-          </span>
-        );
-      })}
-       */}
+    <div className="ted-App">
+     
 
        <div id="flap">
          <span id="head-text">TED</span>
@@ -105,6 +76,9 @@ export default function App() {
        <div id="theme">
          <h1>Themes</h1>
          <hr/>
+         <p>
+            Pick out a theme to view the best Ted Talks of the same!
+         </p>
        </div>
        <nav id="nav">
           <ul>
