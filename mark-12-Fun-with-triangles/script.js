9c969931fb1ccb663ad2c6b21c4eaa7e42ba72d2
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-09-21 03:24:17 +0530

    Update script.js

--- mark-12-Fun-with-triangles/script.js
+++ mark-12-Fun-with-triangles/script.js
@@ -6,8 +6,12 @@
 
  
  function calculateSumOfAllAngles(angle1, angle2, angle3 ){
-
-    const sumofangles = Number(angle1) + Number(angle2)+ Number(angle3);
+    let a= Number(angle1), b= Number(angle2), c= Number(angle3);
+    if(a<=0 || b<=0 || c<=0){
+      output.innerText = "Invalid Input!";
+      return 0;
+    }
+    const sumofangles =  a + b + c;
     return sumofangles;
  }
 
@@ -16,15 +20,17 @@
      
     const sumOfangles =calculateSumOfAllAngles(input[0].value, input[1].value, input[2].value);
     console.log(sumOfangles);
-    if(sumOfangles === 180){
-        
-        output.innerText = "The angles form a Triangle!";
+   if(sumOfangles >0){
+     if(sumOfangles === 180){
 
-    } else {
+         output.innerText = "The angles form a Triangle!";
 
-        output.innerText = "The angles don't form a Triangle!";
+     } else {
 
-    }
+         output.innerText = "The angles don't form a Triangle!";
+
+     }
+   }
 
  }
 
